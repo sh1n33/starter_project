872ce518b7fc4f76b9a02ad539924914f186856e
sh1n33 <shineerdeneshinelunda@gmail.com>
2025-10-26 17:40:53 +0800

    last 10/26

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to Online Shop</h1>
+      <h1 className="text-yellow-300">Welcome to Online Shop</h1>
       <div className="min-h-screen bg-black text-white w-screen">
         <div className="w-full h-48 bg-white">ez</div>
       </div>
